2354406cbb37b1f41dc68699cd7f22cdf65fb44a
Za Wilgustus <za@f5.com>
2016-11-08 20:56:23 +0000

    wip

--- docs/howtos/run_tempest_api_tests.rst
+++ docs/howtos/run_tempest_api_tests.rst
@@ -3,6 +3,8 @@ Clone the Subject Code
 
 #. cd ~
 #. `git clone -b DEVELOPMENT git@github.com:F5Networks/SUBJECTCODE.git`
+    NOTE: DEVELOPMENT=liberty ;; SUBJECTCODE=f5-openstack-lbaasv2-driver
 #. Add ssh key "id_rsa_testlab" to ~/.ssh
 #. Add ssh key "id_rsa_bldrgit" to ~/.ssh
-#. pip install 
+#. pip install git+https://github.com/zancas/prodactivity.git
+#. prodactivity
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='prodactivity',
-    version='0.1.dev0',
+    version='0.1.dev1',
     author='Za Wilgustus',
     author_email='za@f5.com',
     license='Apache 2.0',
@@ -13,5 +13,7 @@ setup(
     scripts=['cli/prodactivity'],
     install_requires=[
             'jinja2 >= 2.8, < 3'
-        ]
+        ],
+    entry_points={'console_scripts':
+        ['prodact_contmanager=prodactivity.testrunners.manager:main']}
 )
